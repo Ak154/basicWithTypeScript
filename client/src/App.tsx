62a03c5ca3cd04b4496088ff6e9a13e5b45ac499
Ak154 <anandkumar15446@gmail.com>
2025-07-15 13:45:46 +0530

    Working on decks page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type TDeck = {
+  title: string;
+  _id: string;
 }
 
-export default App
+function App() {
+  const [decks, setDecks] = useState<TDeck[]>([]);
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await fetch("http://localhost:5000/decks", {
+      method: "POST",
+      body: JSON.stringify({ title }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setTitle("");
+  };
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:5000/decks");
+      const data = await response.json();
+      setDecks(data.data);
+    })();
+  }, []);
+
+  return (
+    <div className="App">
+      <ul className="decks">
+        {decks.map((decks) => (
+          <li key={decks._id}>{decks.title}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="deck-title">Deck Title</label>
+        <input
+          id="deck-title"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button type="submit">add</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,41 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import "./config/dbConfig";
-import Deck from "./models/Deck"
+import Deck from "./models/Deck";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.post("/decks", async(req: Request, res: Response)=>{
-    const newDeck = new Deck({
-        title: req.body.title
-    })
-    const createDeck = await newDeck.save();
-    res.json(createDeck)
-})
+app.get("/decks", async (req: Request, res: Response) => {
+  const deck = await Deck.find({});
 
-app.get("/", (req, res)=>{
-    res.send("Hello world")
-})
+  res
+    .status(200)
+    .json({ message: "Data fetched successfully", success: false, data: deck });
+});
+
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createDeck = await newDeck.save();
+  res.json(createDeck);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 const port = process.env.PORT || 5000;
 
 app.use((req, res) => {
-    res.status(404).send("Route not found");
+  res.status(404).send("Route not found");
 });
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port number ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port number ${port}`);
+});
